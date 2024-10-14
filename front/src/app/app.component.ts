e06faa1f3c3b8da96f3239321ef0adf874bff36c
Baptiste <contact@baptiste-jullion.dev>
2024-10-14 16:23:51 +0200

    feat: display CreateNote input & Note Cards list on main page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { Note } from "./components/Card/Note.component";
+import { CreateNote } from "./components/Input/CreateNote.component";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, CreateNote, Note, CommonModule],
 	templateUrl: "./app.component.html",
-	styleUrl: "./app.component.css",
 })
 export class AppComponent {
-	title = "front";
+	notes: {
+		title: string;
+		content: string;
+	}[] = [];
+
+	loadNotes() {
+		this.notes = JSON.parse(localStorage.getItem("notes") || "[]");
+	}
+
+	ngOnInit() {
+		this.loadNotes();
+	}
 }
