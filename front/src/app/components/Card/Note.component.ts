b219a82e6f6ff94fe653a846bae59294fb958885
Baptiste <contact@baptiste-jullion.dev>
2024-10-15 13:23:25 +0200

    feat: add interactions

--- front/src/app/components/Card/Note.component.ts
+++ front/src/app/components/Card/Note.component.ts
@@ -1,20 +1,41 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 import {
 	MatCard,
+	MatCardContent,
 	MatCardSubtitle,
 	MatCardTitle,
 	MatCardTitleGroup,
-    MatCardContent
 } from "@angular/material/card";
+import { MatIcon } from "@angular/material/icon";
+import type { Note as NoteType } from "../../../../../shared/types/Note";
+// biome-ignore lint/style/useImportType: <explanation>
+import { NoteService } from "../../services/Note.service";
 
 @Component({
 	standalone: true,
 	selector: "note",
 	styles: ":host { display: contents; }",
-	imports: [MatCard, MatCardTitleGroup, MatCardTitle, MatCardSubtitle, MatCardContent],
+	imports: [
+		MatCard,
+		MatCardTitleGroup,
+		MatCardTitle,
+		MatCardSubtitle,
+		MatCardContent,
+		MatButtonModule,
+		MatIcon,
+	],
 	templateUrl: "./Note.component.html",
 })
 export class Note {
-	@Input() title!: string;
-	@Input() content!: string;
+	@Input() note!: NoteType;
+
+	@Output() noteDeleted = new EventEmitter<void>();
+
+	constructor(private noteService: NoteService) {}
+
+	async deleteTask(id: string) {
+		await this.noteService.deleteNote(id);
+		this.noteDeleted.emit();
+	}
 }
