370f3b9a18d86298b1a7db321f668889e0433476
Baptiste <contact@baptiste-jullion.dev>
2024-11-16 18:12:48 +0100

    feat: Add CORS middleware and increase JSON payload limit in Express app

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import { connectDB } from "@db/index";
 import router from "@r/router";
+import cors from "cors";
 import express from "express";
 
 connectDB();
@@ -12,7 +13,8 @@ if (!port) {
 }
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
 
 app.use("/api", router);
 
