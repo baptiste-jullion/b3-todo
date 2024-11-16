6e08c859f07334b09ad2fe64ddf0071e132e76cb
Baptiste <contact@baptiste-jullion.dev>
2024-11-16 20:51:14 +0100

    feat: Update app layout with theme provider and dark mode support

--- front/app.vue
+++ front/app.vue
@@ -1,10 +1,27 @@
 <template>
-  <n-card>
-    <n-button>naive-ui</n-button>
-    <n-input />
-  </n-card>
+  <n-config-provider
+    :theme-overrides="{
+      common: {
+        borderRadius: '6px',
+      },
+    }"
+    :theme="darkTheme"
+  >
+    <n-message-provider>
+      <n-modal-provider>
+        <NuxtPage />
+      </n-modal-provider>
+    </n-message-provider>
+  </n-config-provider>
 </template>
 
 <script setup lang="ts">
-import { NButton, NCard, NInput } from "naive-ui";
+import { NConfigProvider, NMessageProvider, NModalProvider, darkTheme } from "naive-ui";
+
 </script>
+
+<style>
+body {
+  background: rgb(16, 16, 20);
+}
+</style>
