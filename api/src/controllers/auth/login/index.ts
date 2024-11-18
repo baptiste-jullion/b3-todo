5aa783b566c8435179226b24f061402da62fed9d
Baptiste <contact@baptiste-jullion.dev>
2024-11-18 15:27:44 +0100

    feat: Implement login functionality with JWT authentication and password validation

---
+++ api/src/controllers/auth/login/index.ts
@@ -0,0 +1,36 @@
+import User from "@m/User";
+import { APIError, type TypedRequest } from "@u";
+import bcrypt from "bcrypt";
+import type { Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const login = async (
+	req: TypedRequest<{
+		email: string;
+		password: string;
+	}>,
+	res: Response,
+) => {
+	const { email, password } = req.body;
+
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			throw new APIError(404, "Email or password is invalid");
+		}
+
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+
+		if (!isPasswordValid) {
+			throw new APIError(404, "Email or password is invalid");
+		}
+
+		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
+			expiresIn: "1h",
+		});
+
+		res.json({ token });
+	} catch (error) {
+		APIError.handleError(res, error);
+	}
+};
