1f535bbb6f9447fa6635d81fe89b4549c0ae2743
Baptiste <contact@baptiste-jullion.dev>
2024-11-18 09:50:29 +0100

    fix: reorder import statements

--- front/components/Note/View/Grid.vue
+++ front/components/Note/View/Grid.vue
@@ -3,15 +3,15 @@
     <NoteViewGridColumn v-for="state in states" :key="state" :state>
       <template #before v-if="state === 'todo'">
         <NoteViewGridAddButton />
-  </template>
+      </template>
     </NoteViewGridColumn>
   </section>
 </template>
 
 <script setup lang="ts">
 import type { INoteRead } from "@b3-todo/api";
-import NoteViewGridColumn from "~/components/Note/View/Grid/Column.vue";
 import NoteViewGridAddButton from "~/components/Note/View/Grid/AddButton.vue";
+import NoteViewGridColumn from "~/components/Note/View/Grid/Column.vue";
 
 const states = ["todo", "in_progress", "completed"] as INoteRead["state"][];
 </script>
