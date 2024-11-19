238081b1365eb57cd900c70590dfb6b691108de9
Baptiste <contact@baptiste-jullion.dev>
2024-11-19 11:53:52 +0100

    feat: Implement JWT authentication middleware for API routes

---
+++ api/src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import User from "@m/User";
+import { APIError } from "@u";
+import type { NextFunction, Response, Request } from "express";
+import jwt from "jsonwebtoken";
+
+export default async (
+	req: Request & { userId?: string },
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		if (!req.headers.authorization?.startsWith("Bearer"))
+			throw new APIError(401);
+
+		const token = req.headers.authorization.split(" ")[1];
+		const decoded = jwt.verify(
+			token,
+			process.env.JWT_SECRET as string,
+		) as jwt.JwtPayload;
+
+		const user = await User.findById(decoded.id);
+
+		if (!user) throw new APIError(401);
+
+		req.userId = user._id;
+
+		next();
+	} catch (err) {
+		APIError.handleError(res, err);
+	}
+};
