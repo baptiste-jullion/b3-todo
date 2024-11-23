23145aa85c8a5d2edd71a01dd8f76a4c51269fa0
Baptiste <contact@baptiste-jullion.dev>
2024-11-23 19:45:00 +0100

    feat: Rename display_name to username in User model and update type exports

--- api/src/types.ts
+++ api/src/types.ts
@@ -1,3 +1,4 @@
 export type { INoteRead, INoteWrite } from "@m/Note";
 export type { ITagRead, ITagWrite } from "@m/Tag";
 export type { ITask } from "@m/Task";
+export type { IUserRead, IUserWrite } from "@m/User";
