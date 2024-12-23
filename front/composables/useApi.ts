3b017d8e1ffcfb9ff88baf3297cdb0dc33ffded5
Baptiste <contact@baptiste-jullion.dev>
2024-12-23 16:30:03 +0100

    feat: refactor API response handling and simplify authentication flow

--- front/composables/useApi.ts
+++ front/composables/useApi.ts
@@ -1,10 +1,32 @@
-import { useCookie, useNuxtApp, useRuntimeConfig } from "#app";
+import { useNuxtApp } from "#app";
+import type {
+	APIResponseError,
+	APIResponseSuccess,
+} from "@b3-todo/api/src/client";
+import useAuth from "~/composables/useAuth";
 
 export default function useApi() {
 	const { $api } = useNuxtApp();
-	const cookie = useCookie(useRuntimeConfig().public.JWT_COOKIE_NAME);
-	cookie.value && $api.auth.injectToken(cookie.value);
+	const { disconnect } = useAuth();
+
+	const handleAPIResponse = async <T>(
+		response: APIResponseSuccess<T> | APIResponseError,
+	) => {
+		if (response.success) {
+			return response.data;
+		}
+
+		switch (response.response.status) {
+			case 401:
+				await disconnect();
+				break;
+			default:
+				throw new Error(response.error);
+		}
+	};
+
 	return {
 		api: $api,
+		handleAPIResponse,
 	};
 }
--- front/composables/useAuth.ts
+++ front/composables/useAuth.ts
@@ -1,23 +1,12 @@
-import { navigateTo, useCookie, useRuntimeConfig } from "#app";
-import useApi from "~/composables/useApi";
+import { navigateTo } from "#app";
 
 export default function useAuth() {
-	
-		const clearToken = () => {
-			const { JWT_COOKIE_NAME } = useRuntimeConfig().public;
-			const token = useCookie(JWT_COOKIE_NAME);
-			token.value = "";
-		}
-	
 	const disconnect = async () => {
-		const { api } = useApi();
-		await api.auth.logout();
-		clearToken();
+		localStorage.removeItem("access-token");
 		navigateTo("/auth");
 	};
 
 	return {
 		disconnect,
-		clearToken,
 	};
 }
