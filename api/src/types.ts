1695eed55153f0c6f5f7e3d88c1008ed8eaa5caa
Baptiste <contact@baptiste-jullion.dev>
2024-12-26 14:43:27 +0100

    feat: update task type exports to include read and write variants

--- api/src/types.ts
+++ api/src/types.ts
@@ -1,6 +1,6 @@
 export type { INoteRead, INoteWrite } from "@m/Note";
 export type { ITagRead, ITagWrite } from "@m/Tag";
-export type { ITask } from "@m/Task";
+export type { ITaskRead, ITaskWrite } from "@m/Task";
 export type { IUserRead, IUserWrite } from "@m/User";
 
 declare global {
@@ -13,7 +13,7 @@ declare global {
 			API_UPLOADS_ROUTE: string;
 			API_PROTOCOL: string;
 			JWT_ACCESS_SECRET: string;
-      JWT_REFRESH_SECRET: string;
+			JWT_REFRESH_SECRET: string;
 			PORT: string;
 			NUXT_PUBLIC_UPLOADS_BASE_URL: string;
 			NUXT_PUBLIC_API_BASE_URL: string;
